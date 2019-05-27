0922b27d19caa641215f18b0d4b6ba169d28ff01
wjl <wangjl204@163.com>
2019-05-27 16:58:26 +0800

    修改数据库配置

--- config.inc.php
+++ config.inc.php
@@ -12,9 +12,9 @@ $my_openid = "oKzdw1cb2uh3Ow5Wd-M9OMB2ObZw";
 //$db_name = "ws_wx";
 //$db_port = 3306;
 
-$db_host = "192.168.100.82";
-$db_user = "wjl";
-$db_pwd = "Wjl@Hhy1314";
+$db_host = "localhost";
+$db_user = "root";
+$db_pwd = "mudj3OD4";
 $db_name = "OrderMan";
 $db_port = 3306;
 
